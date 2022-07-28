b0edd980904900c925a46024703b68a2906fbce6
asherk7 <masherk03@hotmail.com>
2022-07-27 22:47:11 -0400

    added more to the web scraper

--- web_scraper.py
+++ web_scraper.py
@@ -1,14 +1,12 @@
-"""https://eatdrinkandsleepfootball.com/schedule/strength-of-schedule.html
-https://rapidapi.com/DathanStoneDev/api/nfl-team-stats/
-use total spending for offense/defense, spending for 
-crucial positions(corner, linebacker, QB, WR), record from the previous season, 
-stats from last season(passing, receiving, rushing), schedule difficulty"""
-
+"""
+Web scraping code
+"""
 from bs4 import BeautifulSoup
 import requests
 import csv
+import json
 
-def projections():
+def projections(): #finding bleacherreports predictions for team records in 2022-2023
     projections = requests.get("https://bleacherreport.com/articles/10042148-overunder-win-predictions-for-every-nfl-team-in-2022").text
     soup = BeautifulSoup(projections, 'lxml')
 
@@ -23,7 +21,7 @@ def projections():
             teams.append(team.h1.text)
     for record in soup.find_all('ol'):
         try:
-            wincount = record.find_all('p', class_='')[1].text
+            wincount = record.find_all('p', class_='')[1].text #if multiple lines in one section, use indexing to grab a specific one
             if 'Over/Under' in wincount:
                 win = ''
                 for letter in wincount:
@@ -38,7 +36,7 @@ def projections():
 
     csv_file.close()
 
-def total_spending():
+def total_spending(): #finding how much each team spent on each position
     money_spent = requests.get('https://overthecap.com/positional-spending').text
     soup = BeautifulSoup(money_spent, 'lxml')
 
@@ -66,7 +64,7 @@ def total_spending():
 
     csv_file.close()
 
-def win_loss():
+def win_loss(): #finding each teams record in the previous season
     win_loss = requests.get('https://www.teamrankings.com/nfl/trends/win_trends/?sc=is_regular_season').text
     soup = BeautifulSoup(win_loss, 'lxml')
 
@@ -84,12 +82,46 @@ def win_loss():
     
     csv_file.close()
 
-def strength_of_schedule():
+def strength_of_schedule(): #finding each teams strength of schedule in 2022-2023
     strength_of_schedule = requests.get('https://eatdrinkandsleepfootball.com/schedule/strength-of-schedule.html').text
     soup = BeautifulSoup(strength_of_schedule, 'lxml')
 
-    #csv_file = open('strength_of_schedule.csv', 'w', newline = '')
-    #csv_writer = csv.writer(csv_file)
-    #csv_writer.writerow(['Team', 'Strength of Schedule']) 
+    csv_file = open('strength_of_schedule.csv', 'w', newline = '')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Team', 'Strength of Schedule']) 
 
-    parser = soup.find()
+    parser = soup.find('div', id='content')
+    for team in parser.find_all('tr'):
+        team_strength = [team.find_all('td')[1].text, team.find_all('td')[3].text]
+        csv_writer.writerow(team_strength)
+    
+    csv_file.close()
+
+def team_passing(): #finding the passing stats of each team
+    csv_file = open('team_passing.csv', 'w', newline = '')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Team', 'Passing Yards', 'Touchdowns'])
+
+    #using a football API to get JSON data for team passing stats
+    headers = {"X-RapidAPI-Key": "c2c317b721msh130b59a39209c44p1e3a6fjsnc8102dec7b4d",
+	    "X-RapidAPI-Host": "nfl-team-stats.p.rapidapi.com"}
+    response = requests.get('https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/passing-stats/offense/2021', headers=headers).text
+    
+    #taking the JSON response in string form, and turning it into a python dictionary
+    json_data = json.loads(response)
+    
+    #parsing through the JSON data to turn it into a CSV file
+    for i in json_data:
+        team_stats = [i['name'], i['passYards'], i['touchdowns']]
+        csv_writer.writerow(team_stats)
+
+    csv_file.close()
+
+def team_receiving():
+    pass
+
+def team_rushing():
+    pass
+
+def turnovers():
+    pass
