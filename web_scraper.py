4df9ecbbfea70f65f12288f3b8825e54c9171c5b
asherk7 <masherk03@hotmail.com>
2022-08-01 17:20:26 -0400

    finished scraping data

--- web_scraper.py
+++ web_scraper.py
@@ -10,6 +10,7 @@ def projections(): #finding bleacherreports predictions for team records in 2022
     projections = requests.get("https://bleacherreport.com/articles/10042148-overunder-win-predictions-for-every-nfl-team-in-2022").text
     soup = BeautifulSoup(projections, 'lxml')
 
+    #created a csv file to add the data to
     csv_file = open('projections.csv', 'w', newline = '')
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['Team', 'Record Prediction'])
@@ -17,6 +18,7 @@ def projections(): #finding bleacherreports predictions for team records in 2022
     teams = []
     records = []
     for team in soup.find_all('div', class_='organism contentStream slide'):
+        #finding each team name and adding it
         if len(team.h1.text.split()) <= 3:
             teams.append(team.h1.text)
     for record in soup.find_all('ol'):
@@ -44,13 +46,13 @@ def total_spending(): #finding how much each team spent on each position
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(['Team', 'QB', 'RB', 'WR', 'OL', 'Offense', 'DL', 'LB', 'S', 'CB', 'Defense'])
 
-    year = soup.find('div', id='y2022')
+    year = soup.find('div', id='y2022') #finding the specific section for the year
     for team in year.find_all('tr', class_='sortable'):
         Team_data = []
         counter = 0
         linemen = 0
         for money in team.find_all('td'):
-            if counter == 4:
+            if counter == 4: #using a counter to find the money for certain positons
                 pass
             elif counter == 7 or counter == 8:
                 linemen += int(money.text.strip('$').replace(',', ''))
@@ -64,7 +66,7 @@ def total_spending(): #finding how much each team spent on each position
 
     csv_file.close()
 
-def win_loss(): #finding each teams record in the previous season
+def win_loss(): #finding each teams win percentage in the previous season
     win_loss = requests.get('https://www.teamrankings.com/nfl/trends/win_trends/?sc=is_regular_season').text
     soup = BeautifulSoup(win_loss, 'lxml')
 
@@ -76,7 +78,7 @@ def win_loss(): #finding each teams record in the previous season
     for team in parser.find_all('tr'):
         team_data = []
         for data in team.find_all('td'):
-            if (data.text.replace(' ', '').isalpha()) or ('%' in data.text):
+            if (data.text.replace(' ', '').isalpha()) or ('%' in data.text): #checking for each certain condition within the html code
                 team_data.append((data.text).strip('%'))
         csv_writer.writerow(team_data)
     
@@ -92,7 +94,7 @@ def strength_of_schedule(): #finding each teams strength of schedule in 2022-202
 
     parser = soup.find('div', id='content')
     for team in parser.find_all('tr'):
-        team_strength = [team.find_all('td')[1].text, team.find_all('td')[3].text]
+        team_strength = [team.find_all('td')[1].text, team.find_all('td')[3].text] #find_all returns a list, so indexing is used to find the specific values
         csv_writer.writerow(team_strength)
     
     csv_file.close()
@@ -117,31 +119,58 @@ def team_receiving(): #finding the receiving stats of each team
 
     csv_file.close()
 
-def team_passing():
+def team_passing(): #finding advanced passing stats of each team
     source = requests.get('https://www.nfl.com/stats/team-stats/offense/passing/2021/reg/all').text
     soup = BeautifulSoup(source, 'lxml')
 
-    #csv_file = open('team_passing.csv', 'w', newline = '')
-    #csv_writer = csv.writer(csv_file)
-    #csv_writer.writerow(['Team', 'QBR', 'Completion Percentage', 'Y/A', 'Interceptions', 'Sacks', 'Sack Yards']) 
+    csv_file = open('team_passing.csv', 'w', newline = '')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Team', 'Completion Percentage', 'Y/A', 'QBR']) 
 
-    parser = soup.find()
+    parser = soup.find('tbody')
+    turnover = []
+    for team in parser.find_all('tr'):
+        stats = []
+        stats.append(team.find('div', class_='d3-o-club-fullname').text)
+        team_stats = team.find_all('td')
+        stats.extend([team_stats[3].text, team_stats[4].text, team_stats[8].text])
+        turnover.append([team.find('div', class_='d3-o-club-fullname').text, team_stats[7].text, team_stats[-2].text, team_stats[-1].text])
+        csv_writer.writerow(stats)
 
-    #csv_file.close()
+    csv_file.close()
+    #return turnover (used for the turnover function)
 
-def team_rushing():
+def team_rushing(): #finding the rushing stats of each team
     source = requests.get('https://www.nfl.com/stats/team-stats/offense/rushing/2021/reg/all').text
     soup = BeautifulSoup(source, 'lxml')
 
-    #csv_file = open('team_rushing', 'w', newline = '')
-    #csv_writer = csv.writer(csv_file)
-    #csv_writer.writerow(['Team', 'Rushing Yards', 'Touchdowns', 'YPC', 'Fumbles']) 
+    csv_file = open('team_rushing.csv', 'w', newline = '')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Team', 'Rushing Yards', 'YPC', 'Touchdowns']) 
 
-    parser = soup.find()
+    parser = soup.find('tbody')
+    turnover = []
+    for team in parser.find_all('tr'):
+        stats = []
+        stats.append(team.find('div', class_='d3-o-club-fullname').text)
+        team_stats = team.find_all('td')
+        stats.extend([team_stats[2].text, team_stats[3].text, team_stats[4].text])
+        turnover.append([team.find('div', class_='d3-o-club-fullname').text, team_stats[10].text])
+        csv_writer.writerow(stats)
 
-    #csv_file.close()
+    csv_file.close()
+    #return turnover (used for the turnover function)
 
-def turnovers(interceptions, fumbles, sacks, sack_yards):
-    #send data here from team passing and team rushing
-    #sacks aren't turnovers but they reduce offensive efficiency
-    pass
+def turnovers(): #finding each teams turnovers and offensive blunders
+    passing = team_passing()
+    rushing = team_rushing()
+
+    csv_file = open('turnovers.csv', 'w', newline = '')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Team', 'Interceptions', 'Sacks', 'Sack Yards', 'Fumbles'])
+    
+    for p, r in zip(passing, rushing):
+        p.extend([r[1]])
+        csv_writer.writerow(p)
+
+    csv_file.close()
