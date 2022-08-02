659ac77ac52dc18e508d0be463f94a96856bd3b1
asherk7 <masherk03@hotmail.com>
2022-08-02 19:56:08 -0400

    updated flask app and html

--- app.py
+++ app.py
@@ -6,37 +6,62 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prediction.db' #creates datab
 db = SQLAlchemy(app) #initializes database with file
 
 class Todo(db.Model): #creating a database for user record prediction
-    record = db.Column(db.String(200), primary_key = True, nullable = False)
-    team = db.Column(db.String(30))
+    id = db.Column(db.Integer, primary_key=True)
+    record = db.Column(db.String(200), nullable = False)
 
     def __repr__(self): #returns the record everytime a new element is made
-        return '<Record %r>' % self.record
+        return '<prediction %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index(): #creating main route
     if request.method == 'POST':
-        return redirect('/records/')
+        if request.form['redirect'] == 'predictions':
+            return redirect('/records')
+        else:
+            return redirect('/ml_info')
 
     else:
         return render_template('main.html')
 
-@app.route('/records/', methods=['POST', 'GET']) #methods allow us to send data to webpage
+@app.route('/ml_info', methods=['POST', 'GET'])
+def machinelearning():
+    if request.method == 'POST':
+        return redirect('/')
+    else:
+        return render_template('ML_info.html')
+
+@app.route('/records', methods=['POST', 'GET']) #methods allow us to send data to webpage
 def records():
     if request.method == 'POST': #if the user is posting something
-        user_content = request.form['prediction'] #id/name of the input
-        user_prediction = Todo(record=user_content) #creating an object
+        if request.form['redirect'] == 'homepage':
+            return redirect('/')
+        else:
+            user_content = request.form['prediction'] #id/name of the input
+            user_prediction = Todo(record=user_content) #creating an object
 
-        try:
-            db.session.add(user_prediction) #adding user input to the database session
-            db.session.commit()
-            return redirect('/records/') #redirecting back to the same page
-        except:
-            return 'There was an issue adding the prediction'
+            try:
+                db.session.add(user_prediction) #adding user input to the database session
+                db.session.commit()
+                return redirect('/records') #redirecting back to the same page
+            except:
+                return 'There was an issue adding the prediction'
 
     else:
-        predictions = Todo.query.all() #grabs all the past predictions
+        predictions = Todo.query.order_by(Todo.record).all() #grabs all the past predictions
         return render_template('record.html', predictions=predictions) #sends past inputs into the page to use
 
+@app.route('/delete/<int:id>') #routes to delete, and database's id
+def delete(id):
+    prediction_delete = Todo.query.get_or_404(id) #grabbing the unique id
+
+    try:
+        db.session.delete(prediction_delete)
+        db.session.commit()
+        return redirect('/records')
+    except:
+        return 'There was a problem deleting that prediction'
+
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
     app.run(debug=True)
-    #db.create_all()
